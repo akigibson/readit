74c64f53867753e261c82ed102f853ab8a58ca36
MakerSquare Student <students@themakersquare.com>
2013-07-08 15:44:37 -0500

    created a new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Readit::Application.config.secret_token = '27e57a3d285523538bc27249e7adbda6084166c426227bed402f84aa09d6034726ab78619b6eb527b79268adb6b36e6ab2a4c40556bb8a6b1d7f9beaf95ace2c'
