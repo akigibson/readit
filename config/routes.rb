998ac17eb08573d8f352f2560a0b19401beb3d1e
MakerSquare Student <students@themakersquare.com>
2013-07-08 16:43:56 -0500

    completed assignment

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Readit::Application.routes.draw do
+  devise_for :users
+
   resources :pages
   root to: "pages#index"
+
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
